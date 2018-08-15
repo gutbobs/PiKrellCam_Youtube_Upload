fdd459998e5d36a47f45d51fb95219e66d9531b1
gutbobs <gutbobs>
2018-08-15 18:47:12 +0100

    added option to send email alert on upload

--- queue_monitor.py
+++ queue_monitor.py
@@ -3,8 +3,12 @@ import os
 import time
 import subprocess
 import urllib.parse
+import smtplib
 from modules import database
 from modules import load_variables
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 class main:
 	def __init__(self):
@@ -15,6 +19,10 @@ class main:
 		self.cam_id = 1
 		self.retries_before_ignore = 3
 		self.loop_time = 5
+		self.email_on_upload="yes"
+		self.email_to="root@localhost"
+		self.email_from="root@localhost"
+		self.email_server="localhost"
 		
 	def	Initialise_DB(self):
 		self.DB = database.MySQLdb()
@@ -23,6 +31,17 @@ class main:
 		self.DB.dbpassword = self.db_passwd
 		self.DB.db = self.db_name
 		
+	def send_email(self,to,subject,body):
+		msg = MIMEMultipart()
+		msg['Subject'] = subject
+		msg['From'] = "root"
+		msg['To'] = to
+
+		msg.attach(MIMEText(body, 'plain'))
+		server= smtplib.SMTP(self.email_server)
+		server.sendmail(self.email_from,self.email_to,msg.as_string())
+		server.quit
+        
 	def check_queue(self):
 		sql_cmd = """select `INDEX`,`DATEANDTIME`,`VIDEONAME` 
 		from {db_name}.VideoData
@@ -45,11 +64,14 @@ class main:
 				self.DB.Update(sql_cmd)
 				continue
 
+			print ("Uploading %s" % item['VIDEONAME'])
 			cmd_line = 'sudo python /opt/youtube_uploader/upload_video.py --file="%s" --title="%s" --description="%s" --privacyStatus="private"' % (filename,title,description)
 			p = subprocess.Popen(cmd_line,shell=True,stdout=subprocess.PIPE)
 			cmd_result = p.communicate()
 			#print (cmd_result)
 			#print (p.returncode)
+
+                       
 			if (p.returncode == 0):
 				sql_cmd = """UPDATE `VideoData`
 				set `UPLOADED`='1', `UPLOADATTEMPTS`='%s'
@@ -63,6 +85,12 @@ class main:
 			sql_cmd = "UPDATE `VideoData` set `UPLOADRESULT`='{cmd_res}',`UPLOADED`='1' where `INDEX`='{index}' ;".format(cmd_res=urllib.parse.quote_plus(cmd_result[0]),index=item['INDEX'])
 			#print (sql_cmd)
 			self.DB.Update(sql_cmd)
+
+			if self.email_on_upload == "yes":
+				subject = "PiKRellCam Video Uploaded"
+				body = "A video has been uploaded to youtube. The cmd result was %s" % cmd_result[0]
+				print ("Sending Email")
+				self.send_email(self.send_to,subject,body)			
 
 		
 		
@@ -78,6 +106,10 @@ if __name__ == "__main__":
 	queue_monitor.db_passwd = variables['database_password']
 	queue_monitor.cam_id = variables['camera_id']
 	queue_monitor.retries_before_ignore = variables['retries_before_fail']
+	queue_monitor.email_on_upload = variables['email_on_upload']
+	queue_monitor.email_to = variables['email_to']
+	queue_monitor.email_from = variables['email_from']
+	queue_monitor.email_server = variables['email_server']
 	
 	queue_monitor.Initialise_DB()
 	
